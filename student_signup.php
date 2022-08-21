91acbcd35841e33fba1b386e71317d33946e703f
Zyan <fislam183070@bscse.uiu.ac.bd>
2022-08-22 00:14:39 +0600

    Create student signup.php

---
+++ student_signup.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign Up | Archeus</title>
+
+    <link rel="stylesheet" href="welcome.css">
+
+
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+
+    <script src="https://code.jquery.com/jquery-3.1.1.min.js" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.1.1.min.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.js"></script>
+
+</head>
+
+<body>
+    <div class="layout">
+        <div class="top">
+            <div>
+                Archeus
+            </div>
+        </div>
+
+        <div class="mid">
+
+        </div>
+
+        <div class="bottom">
+            <div>
+                Developed by
+            </div>
+            <div>
+                Deadly Diamond Of Death | 2022
+            </div>
+        </div>
+    </div>
+
+
+
+</body>
+
+</html>
