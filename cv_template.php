bb059d8692ffc9d3323e56e5eab9bf67216fb95c
GingerAle <sakib-sadman@users.noreply.github.com>
2022-09-05 18:11:39 +0600

    cv_template

---
+++ cv_template.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CV Template</title>
+
+    <link rel="stylesheet" href="cv_template.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;1,500&family=Quicksand:wght@300;400&display=swap"
+        rel="stylesheet">
+    <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
+</head>
+
+<body class="page">
+    <div class="container ">
+        <div class="row justify-content-center">
+            <div class="col top_part">
+                <div class="row justify-content-center">
+                    <div class="col top_field">
+                        <h3 class="name">Name</h3>
+                        <br>
+                        <h5 class="category">Address <span class="gen_text">Dhaka, Bangladesh</span></h5>
+                        <h5 class="category">Phone <span class="gen_text">+88012345678911</span></h5>
+                        <h5 class="category">E-mail <span class="gen_text">gg@gmail.com</span></h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col">
+                <div id="line"></div>
+                <div id="diamond">
+                    <i class='bx bxs-graduation bx-flip-horizontal bx-xs' id="icons"></i>
+                    <h5 class="title">Education</h5>
+                </div>
+                <div id="line"></div>
+                <span class="subtitle">Secondary School</span>
+                <div class="row">
+                    <div class="col text">Starting Year</div>
+                    <div class="col text">Ending Year</div>
+                </div>
+                <div id="line"></div>
+                <span class="subtitle">Higher School</span>
+                <div class="row">
+                    <div class="col text">Starting Year</div>
+                    <div class="col text">Ending Year</div>
+                </div>
+                <div id="line"></div>
+                <span class="subtitle">University</span>
+                <div class="row">
+                    <div class="col text">Starting Year</div>
+                    <div class="col text">Ending Year</div>
+                </div>
+                <div class="row">
+                    <div class="col text">Department</div>
+                    <div class="col text">Major</div>
+                    <div class="col text">CGPA</div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <div id="line"></div>
+                <div id="diamond">
+                    <i class='bx bx-extension bx-xs' id="icons"></i>
+                    <h5 class="title">Skills</h5>
+                </div>
+                <div id="line"></div>
+                <span class="subtitle">Technical Skills</span>
+                <ul class="text">
+                    <li>Skill name</li>
+                    <li>Related Project</li>
+                    <li><small>Description</small></li>
+                </ul>
+                <span class="subtitle">Soft Skills</span>
+                <ul class="text">
+                    <li>Skill name</li>
+                </ul>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <div id="line"></div>
+                <div id="diamond">
+                    <i class='bx bx-run bx-xs' id="icons"></i>
+                    <h5 class="title">Extracurricular</h5>
+                </div>
+                <div id="line"></div>
+                <ul class="text">
+                    <li>Activity name</li>
+                    <li><small>Description</small></li>
+                    <li><a href="#">Link</a></li>
+                </ul>
+                <span class="subtitle">Soft Skills</span>
+                <ul class="text">
+                    <li>Skill name</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
