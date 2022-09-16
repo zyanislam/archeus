79e42831c60a1467036e21701dbf297ba0c9f10f
Sheikh Fahad Al Islam <89903034+zyanislam@users.noreply.github.com>
2022-09-16 20:46:30 +0600

    update

--- teacher_filter.php
+++ teacher_filter.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!----======== CSS ======== -->
-    <link rel="stylesheet" href="teacher_home.css">
+    <link rel="stylesheet" href="teacher_filter.css">
 
     <!----===== Boxicons CSS ===== -->
     <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
@@ -15,43 +15,27 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    
-    <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"> -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-tagsinput/0.8.0/bootstrap-tagsinput.css" />
+
     <script src="https://code.jquery.com/jquery-3.1.1.min.js" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.1.1.min.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css">
     <script src="https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-tagsinput/0.8.0/bootstrap-tagsinput.js"></script>
-    <script src="custom_tags_input.js"></script>
-    <title>Teacher Home | Archeus</title>
+
+    <title>Filter Page | Archeus</title>
 </head>
 
 <body class="dark">
     <div class="layout">
-
-        <nav class="navbar fixed-top navbar top">
+        <div class="top">
             <div id="top_title">
-                <span class="home_button" onclick="window.location.href = 'teacher_home.php';">Archeus</span>
-            </div>
-            <div id="top_logout">
-                <a class="ui orange button huge" name="logout" onclick="window.location.href = 'teacher_logoutprocess.php';" id="buttonbox2">Logout</a>
-            </div>
-            
-        </nav>
-
-        <!-- <div class="top">
-            <div id="top_title">
-                Archeus
+                <span class="home_button" onclick="window.location.href = 'student_home.php';">Archeus</span>
             </div>
 
             <div id="top_logout">
-                <a class="ui orange button huge" name="logout" onclick="window.location.href = 'teacher_logoutprocess.php';" id="buttonbox2">Logout</a>
+                <a class="ui orange button huge" name="logout" onclick="window.location.href = 'student_logoutprocess.php';" id="buttonbox2">Logout</a>
             </div>
-        </div> -->
+        </div>
 
         <div class="mid">
             <nav class="sidebar open">
@@ -138,85 +122,11 @@
             </nav>
 
             <div class="bg_contentbox">
-
                 <div class="contentbox">
 
-                    <form action="teacher_newpostprocess.php" method="POST" enctype="multipart/form-data">
-
-                        <div class="ui card newpostbox">
-
-                            <p id="write">Write about an upcoming opportunity</p>
-
-                            <input class="inup" type="text" name="post_title" id="post_title" placeholder="Title Of Your Post">
-
-                            <span class="spaceboxv"></span>
-
-                            <textarea class="form-control" placeholder="Describe The Opportunity For The Students" name="newpost" id="newpost" style="height: 80px;"></textarea>
-
-                            <span class="spaceboxv2"></span>
-
-                            <label class="col-xs-3 control-label"><h2>Tags:</h2></label>
-                            <div class="col-xs-8">
-                                <input type="text" id="skills" name="skills" data-role="tagsinput"  />				
-                            </div>
-
-                            <div align="right">
-                                <button class="ui secondary button huge" id="buttonbox1" type="submit" name="submit">Post</button>
-                            </div>
-
-                        </div>
-
-                    </form>
-
-                    <span class="spaceboxv"></span>
-
-                    <!-- here is the feed-post BackEnd Part -->
-                    <?php
-                    try {
-                        $conn = new PDO('mysql:host=localhost:3306;dbname=archeus;', 'root', '');
-                        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-                        $sqlquery = "SELECT * FROM post_teacher ORDER BY tpost_datetime DESC";
-                        $returnobj = $conn->query($sqlquery);
-
-                        if ($returnobj->rowCount() == 0) {
-                            ///no data found
-                            echo "No data found";
-                        } else {
-                            /*<?php echo $row['id'];?>*/
-                            //tpost_id,t_username, t_name,tpost_title, tpost_desc
-                            $tabledata = $returnobj->fetchAll();
-                            foreach ($tabledata as $row) {
-                    ?>
-                                <div class="ui card postbox">
-                                    <div class="content">
-                                        <i class='right floated bx bx-star iconbox' style='color:#343400'></i>
-                                        <div class="header" id="post_title"><?php echo $row['tpost_title']; ?></div>
-                                        <div class="header" id="author_name"><?php echo $row['t_name']; ?> | Author</div>
-
-                                        <div class="description">
-                                            <p id="post_desc"><?php echo $row['tpost_desc']; ?></p>
-                                        </div>
-                                    </div>
-                                </div>
-                    <?php
-                            }
-                        }
-                    } catch (PDOException $ex) {
-                        //if found error forward to login page
-                        // echo"<script>location.assign('welcome.php')</script>";
-                        echo '<script>
-                        alert("Found error");
-                        window.location = "welcome.php";
-                        </script>';
-                    }
-                    ?>
-                    <!-- BackEnd ends here -->
-
                 </div>
-
             </div>
-
+            
         </div>
     </div>
 
@@ -245,6 +155,7 @@
                 modeText.innerText = "Light mode";
             } else {
                 modeText.innerText = "Dark mode";
+
             }
         });
     </script>
