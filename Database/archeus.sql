eb3fccb970f52c135bbc6aa2c8b28ae9c824a9eb
anika <tahsinanika02@gmail.com>
2022-09-18 01:15:09 +0600

    db update

--- Database/archeus.sql
+++ Database/archeus.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 17, 2022 at 09:08 AM
+-- Generation Time: Sep 17, 2022 at 09:14 PM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.2
 
